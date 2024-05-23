23f20a5e7e34e395b177a02138dbac88a6d42cc3
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2024-05-23 15:02:50 +0900

    making tables and adding info from scl file

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title text NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customers_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id  varchar(20) REFERENCES customers(customers_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(20) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+# Скрипт для заполнения данными таблиц в БД Postgres
+
+
+import csv
+import psycopg2
+
+# connect to db
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='b20d5fcd')
+cur = conn.cursor()
+with open("north_data/employees_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    for row in reader:
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+
+
+with open("north_data/customers_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    for row in reader:
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+
+
+with open("north_data/orders_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    for row in reader:
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+conn.commit()
+conn.close()
